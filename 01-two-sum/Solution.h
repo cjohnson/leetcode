ef65bdb5248d8be804a65e8dbfbabf1b136339c4
cjohnson <collin.g.johnson@gmail.com>
2023-11-19 14:34:16 -0500

    Move definition of twoSum to external-class definition, add asserts for leetcode given constraints

--- 01-two-sum/Solution.h
+++ 01-two-sum/Solution.h
@@ -12,19 +12,28 @@ using namespace std;
 
 class Solution {
 public:
-    // O(n^2) naive solution
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (size_t i = 0; i < nums.size(); ++i) {
-            for (size_t j = 0; j < nums.size(); ++j) {
-                if (i == j) continue;
-                if (nums[i] + nums[j] == target) return { static_cast<int>(i), static_cast<int>(j) };
-            }
-        }
-
-        // "You may assume that each input would have exactly one solution"
-        assert(false);
-    }
+    vector<int> twoSum(vector<int>& nums, int target);
 };
+
+vector<int> Solution::twoSum(vector<int> &nums, int target) {
+    // Assert leetcode constraints
+    assert(2 <= nums.size() && nums.size() <= 10e4);
+#ifndef NDEBUG
+    for (int& num : nums) assert(-10e9 <= num && num <= 10e9);
+#endif
+    assert(-10e9 <= target && target <= 10e9);
+
+    // O(n^2) naive solution
+    for (size_t i = 0; i < nums.size(); ++i) {
+        for (size_t j = 0; j < nums.size(); ++j) {
+            if (i == j) continue;
+            if (nums[i] + nums[j] == target) return { static_cast<int>(i), static_cast<int>(j) };
+        }
+    }
+
+    // "You may assume that each input would have exactly one solution"
+    assert(false);
+}
 
 
 #endif //INC_01_TWO_SUM_SOLUTION_H
