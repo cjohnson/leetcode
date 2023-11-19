3692fedfd3c4fbd89906815f2b5a9f09161dd620
cjohnson <collin.g.johnson@gmail.com>
2023-11-19 16:01:04 -0500

    Use the STL to check the count of things

--- 01-two-sum/test.cpp
+++ 01-two-sum/test.cpp
@@ -7,16 +7,6 @@
 
 using namespace std;
 
-bool exactly_one_of(const vector<int>& nums, const int target) {
-    size_t count = 0;
-    for (auto& num : nums) {
-        if (num == target) ++count;
-        if (count > 1) return false;
-    }
-
-    return count == 1;
-}
-
 int main() {
     Solution solution;
 
@@ -28,8 +18,8 @@ int main() {
         auto sol_1 = solution.twoSum(example_1, example_1_target);
 
         assert(sol_1.size() == 2);
-        assert(exactly_one_of(sol_1, 0));
-        assert(exactly_one_of(sol_1, 1));
+        assert(count(sol_1.begin(), sol_1.end(), 0) == 1);
+        assert(count(sol_1.begin(), sol_1.end(), 1) == 1);
         std::cout << "Example 1: PASS" << std::endl;
     }
 
@@ -41,8 +31,8 @@ int main() {
         auto sol_2 = solution.twoSum(example_2, example_2_target);
 
         assert(sol_2.size() == 2);
-        assert(exactly_one_of(sol_2, 1));
-        assert(exactly_one_of(sol_2, 2));
+        assert(count(sol_2.begin(), sol_2.end(), 1) == 1);
+        assert(count(sol_2.begin(), sol_2.end(), 2) == 1);
         std::cout << "Example 2: PASS" << std::endl;
     }
 
@@ -54,8 +44,8 @@ int main() {
         auto sol_3 = solution.twoSum(example_3, example_3_target);
 
         assert(sol_3.size() == 2);
-        assert(exactly_one_of(sol_3, 0));
-        assert(exactly_one_of(sol_3, 1));
+        assert(count(sol_3.begin(), sol_3.end(), 0) == 1);
+        assert(count(sol_3.begin(), sol_3.end(), 1) == 1);
         std::cout << "Example 3: PASS" << std::endl;
     }
 
