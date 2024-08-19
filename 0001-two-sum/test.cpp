5a69c6a1340a9abd8736b39062dedc529842a88e
cjohnson <collin.g.johnson@gmail.com>
2024-08-19 14:16:01 -0400

    Add comma delimiter, print expected result

--- 0001-two-sum/test.cpp
+++ 0001-two-sum/test.cpp
@@ -1,31 +1,41 @@
 #include "Solution.h"
 
-template<typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
-  os << "{ ";
-  for (const T element : vec) {
-    os << element << " ";
+std::ostream &operator<<(std::ostream &os, const std::vector<int> &vec) {
+  os << "[ ";
+  for (size_t i = 0; i < vec.size(); ++i) {
+    if (i > 0) {
+      os << ", ";
+    }
+    os << vec[i];
   }
-  os << "}";
+  os << " ]";
   return os;
 }
 
 int main() {
   Solution solution;
-  std::vector<int> vec, res;
+  std::vector<int> vec, expected, actual;
 
-  vec = { 2, 7, 11, 15 };
-  res = solution.twoSum(vec, 9);
+  vec = {2, 7, 11, 15};
+  actual = solution.twoSum(vec, 9);
+  expected = {0, 1};
   std::cout << "Test case 1\n";
-  std::cout << res << "\n";
+  std::cout << "Actual:   " << actual << "\n";
+  std::cout << "Expected: " << expected << "\n";
+  std::cout << '\n';
 
-  vec = { 3, 2, 4 };
-  res = solution.twoSum(vec, 6);
+  vec = {3, 2, 4};
+  actual = solution.twoSum(vec, 6);
+  expected = {1, 2};
   std::cout << "Test case 2\n";
-  std::cout << res << "\n";
+  std::cout << "Actual:   " << actual << "\n";
+  std::cout << "Expected: " << expected << "\n";
+  std::cout << '\n';
 
-  vec = { 3, 3 };
-  res = solution.twoSum(vec, 6);
+  vec = {3, 3};
+  actual = solution.twoSum(vec, 6);
+  expected = {0, 1};
   std::cout << "Test case 3\n";
-  std::cout << res << "\n";
+  std::cout << "Actual:   " << actual << "\n";
+  std::cout << "Expected: " << expected << "\n";
 }
