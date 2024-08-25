e46e5807d2085436539c2825b259a3bf265a29f3
cjohnson <collin.g.johnson@gmail.com>
2024-08-25 14:40:39 -0400

    Fix numbers

--- cpp/7-reverse-integer/test.cpp
+++ cpp/7-reverse-integer/test.cpp
@@ -21,13 +21,13 @@ int main() {
 
   actual = solution.reverse(120);
   expected = 21;
-  std::cout << "Test case 2\n";
+  std::cout << "Test case 3\n";
   std::cout << "Actual:   " << actual << "\n";
   std::cout << "Expected: " << expected << "\n";
 
   actual = solution.reverse(1534236469);
   expected = 0;
-  std::cout << "Test case 2\n";
+  std::cout << "Test case 4\n";
   std::cout << "Actual:   " << actual << "\n";
   std::cout << "Expected: " << expected << "\n";
 }
